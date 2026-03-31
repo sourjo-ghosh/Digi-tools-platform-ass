58ed91ebaf693841cff0f922d0bbaae3c5b620b2
sourjo-ghosh <joyghosh211@outlook.com>
2026-03-31 06:23:16 +0600

    style: raedy to transform component style fix

--- src/homePages/ReadyToTransform/ReadyTotransform.jsx
+++ src/homePages/ReadyToTransform/ReadyTotransform.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ReadyTotransform = () => {
   return (
-      <div className="flex flex-col justify-center items-center p-20 bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white">
+      <div className="flex flex-col justify-center items-center py-20 px-10 bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white">
           <p className=" font-bold text-4xl ">Ready to Transform Your Workflow?</p>
           <p className="text-[16px] mt-4">Join thousands of professionals who are already using Digitools to work smarter. Start your free trial today.</p>
         <div className="flex gap-2 justify-center items-center mt-6">
